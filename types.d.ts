eec62d3a06714e22519b12c8832ecc3e4af0a679
Kin <riennsuu@gmail.com>
2024-02-03 22:30:16 +0000

    fix command typings

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,10 @@
 import type {
 	APIApplicationCommandInteraction,
 	APIChatInputApplicationCommandInteraction,
+	APIInteractionResponse,
+	APIInteractionResponseChannelMessageWithSource,
+	APIInteractionResponseDeferredChannelMessageWithSource,
+	APIModalInteractionResponse,
 	RESTPostAPIApplicationCommandsJSONBody,
 	RESTPostAPIChatInputApplicationCommandsJSONBody,
 } from "discord_api_types";
@@ -8,15 +12,21 @@ import type {
 export interface ICommand<
 	Data extends RESTPostAPIApplicationCommandsJSONBody,
 	Interaction extends APIApplicationCommandInteraction,
+	ResponseType extends APIInteractionResponse,
 > {
 	data: Data;
-	execute(interaction: Interaction): void | Promise<void>;
+	execute(interaction: Interaction): ResponseType | Promise<ResponseType>;
 }
 
 export type ChatInputCommand = ICommand<
 	& Required<Pick<RESTPostAPIChatInputApplicationCommandsJSONBody, "type">>
 	& RESTPostAPIChatInputApplicationCommandsJSONBody,
-	APIChatInputApplicationCommandInteraction
+	APIChatInputApplicationCommandInteraction,
+	(
+		| APIInteractionResponseChannelMessageWithSource
+		| APIInteractionResponseDeferredChannelMessageWithSource
+		| APIModalInteractionResponse
+	)
 >;
 
 export type Command = ChatInputCommand;
