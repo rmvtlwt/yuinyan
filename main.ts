1db08e2b53416b2c083414a47eaeef66acff0299
Kin <riennsuu@gmail.com>
2024-02-04 12:19:10 +0700

    style: prettier (#8)
    * style: formatting

--- main.ts
+++ main.ts
@@ -20,90 +20,93 @@ import {
 
 async function handler(request: Request): Promise<Response> {
 	const requestUrl = new URL(request.url);
+	const requiredHeaders = [
+		"x-signature-timestamp",
+		"x-signature-ed25519",
+	];
+
 	const invalidRequest = new Response("Invalid request.", {
 		status: STATUS_CODE.Unauthorized,
 	});
 
-	if (request.method != "POST") {
-		return invalidRequest;
-	} else {
-		const requiredHeaders = [
-			"x-signature-timestamp",
-			"x-signature-ed25519",
-		];
-		if (!requiredHeaders.every((header) => request.headers.has(header))) {
+	if (
+		request.method === "POST" &&
+		requiredHeaders.every((header) => request.headers.has(header))
+	) {
+		const body = await request.text();
+		const [timestamp, signature] = requiredHeaders.map((header) =>
+			request.headers.get(header)
+		);
+
+		const valid = tweetnacl.sign.detached.verify(
+			new TextEncoder().encode(timestamp + body),
+			decodeHex(signature!),
+			decodeHex(Deno.env.get("DISCORD_PUBLIC_KEY")!),
+		);
+
+		if (!valid) {
 			return invalidRequest;
 		} else {
-			const body = await request.text();
-			const [timestamp, signature] = requiredHeaders.map((header) =>
-				request.headers.get(header)
+			const rest = new REST().setToken(
+				Deno.env.get("DISCORD_TOKEN")!,
 			);
+			const interaction: APIInteraction = JSON.parse(body);
 
-			const valid = tweetnacl.sign.detached.verify(
-				new TextEncoder().encode(timestamp + body),
-				decodeHex(signature!),
-				decodeHex(Deno.env.get("DISCORD_PUBLIC_KEY")!),
-			);
-
-			if (!valid) {
-				return invalidRequest;
-			} else {
-				const rest = new REST().setToken(
-					Deno.env.get("DISCORD_TOKEN")!,
+			if (isCommand(interaction)) {
+				const command = await manifestGen.commands.find((command) =>
+					command.data.name === interaction.data.name &&
+					command.data.type === interaction.data.type
 				);
-				const interaction: APIInteraction = JSON.parse(body);
+				const opts = { rest };
 
-				if (isCommand(interaction)) {
-					const command = await manifestGen.commands.find((command) =>
-						command.data.name === interaction.data.name &&
-						command.data.type === interaction.data.type
-					);
-					if (command) {
-						if (isChatInput(command)) {
-							return await command.execute(
+				if (command) {
+					if (isChatInput(command)) {
+						return await command.execute({
+							interaction:
 								interaction as APIChatInputApplicationCommandInteraction,
-							);
-						} else {
-							console.log(interaction);
-							return new Response(
-								`Unknowm command type. (${interaction.data.type})`,
-							);
-						}
+							...opts,
+						});
 					} else {
-						const err = `Command not found.`;
-
-						console.log(err);
-						return new Response(err);
-					}
-				}
-				if (isPing(interaction)) {
-					const updateCommands = stringToBoolean(
-						requestUrl.searchParams.get("updateCommands")!,
-					);
-					if (updateCommands) {
-						await rest.put(
-							Routes.applicationCommands(
-								Deno.env.get("DISCORD_ID")!,
-							),
-							{
-								body: manifestGen.commands.map((command) =>
-									command.data
-								),
-							},
+						console.log(interaction);
+						return new Response(
+							`Unknowm command type. (${interaction.data.type})`,
 						);
 					}
-
-					return Response.json({
-						type: InteractionResponseType.Pong,
-					});
 				} else {
-					console.log(interaction);
-					return new Response(
-						`Unknown interaction type. (${interaction.type})`,
+					const err = `Command not found.`;
+
+					console.log(err);
+					return new Response(err);
+				}
+			} else if (isPing(interaction)) {
+				const updateCommands = stringToBoolean(
+					requestUrl.searchParams.get("updateCommands")!,
+				);
+				if (updateCommands) {
+					await rest.put(
+						Routes.applicationCommands(
+							Deno.env.get("DISCORD_ID")!,
+						),
+						{
+							body: manifestGen.commands.map((command) =>
+								command.data
+							),
+						},
 					);
 				}
+
+				return Response.json({
+					type: InteractionResponseType.Pong,
+				});
+			} else {
+				console.log(interaction);
+				return new Response(
+					`Unknown interaction type. (${interaction.type})`,
+				);
 			}
 		}
+	} else {
+		return invalidRequest;
 	}
 }
 
--- types.d.ts
+++ types.d.ts
@@ -1,16 +1,23 @@
 import type {
 	APIApplicationCommandInteraction,
 	APIChatInputApplicationCommandInteraction,
+	APIInteraction,
 	RESTPostAPIApplicationCommandsJSONBody,
 	RESTPostAPIChatInputApplicationCommandsJSONBody,
 } from "discord_api_types";
+import { REST } from "@discordjs/rest";
+
+export interface ExecuteOptions<Interaction extends APIInteraction> {
+	rest: REST;
+	interaction: Interaction;
+}
 
 export interface ICommand<
 	Data extends RESTPostAPIApplicationCommandsJSONBody,
 	Interaction extends APIApplicationCommandInteraction,
 > {
 	data: Data;
-	execute(interaction: Interaction): Response | Promise<Response>;
+	execute(options: ExecuteOptions<Interaction>): Response | Promise<Response>;
 }
 
 export type ChatInputCommand = ICommand<
